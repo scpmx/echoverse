77490ae5696eda96042b346a43dae90b85c0647a
Stephen <13601394+scpmx@users.noreply.github.com>
2024-07-22 19:53:24 -0400

    Install Peerbit and set target to esnext

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script lang="ts">
+
+    import { Peerbit } from "peerbit";
+
+    async function init() {
+        var c1 = await Peerbit.create();
+
+        return c1.peerId.toString();
+    }
+
+</script>
+
+{#await init()}
+    <p>loading...</p>
+{:then x} 
+    <p>{x}</p>
+{/await}
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
