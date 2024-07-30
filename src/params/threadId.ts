441ef2098e1ed2f8da3672963c000aea67f39d45
Stephen <13601394+scpmx@users.noreply.github.com>
2024-07-30 00:10:22 -0400

    Remove threadId params matcher

--- src/params/threadId.ts
+++
@@ -1,7 +0,0 @@
-import type { ParamMatcher } from '@sveltejs/kit';
-
-const guidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
-
-export const match: ParamMatcher = (param) => {
-    return guidRegex.test(param);
-}
