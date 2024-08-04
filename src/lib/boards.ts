8d561972a3e9957cfd2d92b91277c0fb7b2cd39d
Stephen <13601394+scpmx@users.noreply.github.com>
2024-08-04 18:29:31 -0400

    Let's completely rewrite it :-D

--- src/lib/boards.ts
+++
@@ -1,8 +0,0 @@
-type Board = { slug: string, name: string };
-
-export var boards: Board[] = [
-    { slug: "v", name: "Video Games" },
-    { slug: "g", name: "Technology" },
-    { slug: "sci", name: "Science and Math" },
-    { slug: "biz", name: "Business and Finance" }
-];
