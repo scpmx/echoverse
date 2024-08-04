e8fb072f7781b3ee75ff7714e4ee0e0e20084cd0
Stephen <13601394+scpmx@users.noreply.github.com>
2024-08-04 18:40:22 -0400

    Add daisy ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui"
 
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
@@ -7,5 +8,8 @@ export default {
     extend: {}
   },
 
-  plugins: [require("@tailwindcss/typography")]
+  plugins: [
+    daisyui,
+    require("@tailwindcss/typography")
+  ]
 } as Config;
