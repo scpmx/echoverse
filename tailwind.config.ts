cabf134868a67ad24c904edb54345195d1a00b9e
Stephen <13601394+scpmx@users.noreply.github.com>
2024-08-04 18:51:30 -0400

    Set theme to dim and update order ot tailwind plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,45 @@ export default {
     extend: {}
   },
 
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
+
   plugins: [
-    daisyui,
-    require("@tailwindcss/typography")
+    require("@tailwindcss/typography"),
+    daisyui // should be after typography plugin for some reason
   ]
 } as Config;
