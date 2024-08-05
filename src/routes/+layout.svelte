6574991986cc7f3512a5bed4f4e4c66c0b9172ca
Stephen <13601394+scpmx@users.noreply.github.com>
2024-08-04 22:02:48 -0400

    Update project to use adapter static

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
-<script>import "../app.css";</script><slot></slot>
+<script>
+  import "../app.css";
+</script>
+
+<div class="navbar bg-base-900">
+  <a href="#/" class="btn btn-ghost text-xl">SOLOCHAN</a>
+</div>
+
+<slot></slot>
