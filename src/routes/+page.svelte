cbb20a1d558e4f596d7b8728d28f2ee25e3d17a4
Stephen <13601394+scpmx@users.noreply.github.com>
2024-08-06 01:50:06 -0400

    Rename ScrollableContent to MainContent as its a better name maybe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import "../app.css";
   import Sidebar from "$lib/components/Sidebar.svelte";
-  import ScrollableContent from "$lib/components/ScrollableContent.svelte";
-  import ScrollableContentHeader from "$lib/components/ScrollableContentHeader.svelte";
+  import MainContentHeader from "$lib/components/MainContentHeader.svelte";
+  import MainContent from "$lib/components/MainContent.svelte";
 </script>
 
 <div class="flex h-screen">
   <Sidebar />
   <div class="flex-1 flex flex-col">
-     <ScrollableContentHeader />
-     <ScrollableContent/>
+     <MainContentHeader />
+     <MainContent/>
   </div>
 </div>
