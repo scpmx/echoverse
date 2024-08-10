8b8f3be2253f1ec9d91235ddeb2e90a1ad181fc6
Stephen <13601394+scpmx@users.noreply.github.com>
2024-08-10 12:43:27 -0400

    Create database schema for chats and topics

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,37 +1,184 @@
-import { field, variant } from "@dao-xyz/borsh";
-import { Documents, SearchRequest } from "@peerbit/document";
+import { field, option, variant } from "@dao-xyz/borsh";
+import { Documents } from "@peerbit/document";
 import { Program } from "@peerbit/program";
 import { sha256Sync } from "@peerbit/crypto";
 import { v4 as uuid } from "uuid";
+import { concat } from "uint8arrays";
 
-export class SimpleDocument {
+@variant(0)
+export class Message {
+
     @field({ type: "string" })
     id: string;
+
+    @field({ type: option("string") })
+    name?: string;
+
+    @field({ type: "string" })
+    date: string;
 
     @field({ type: "string" })
     content: string;
 
-    constructor(properties: { content: string }) {
+    constructor(
+        date: string,
+        content: string,
+        name?: string
+    ) {
         this.id = uuid();
-        this.content = properties.content;
+        this.name = name;
+        this.date = date;
+        this.content = content;
     }
 }
 
-@variant("example-store")
-export class ExampleStore extends Program {
-    @field({ type: Documents })
-    documents: Documents<SimpleDocument>;
+@variant("chat")
+export class Chat extends Program {
 
-    constructor() {
+    @field({ type: "string" })
+    id: string;
+
+    @field({ type: option("string") })
+    name?: string;
+
+    @field({ type: "string" })
+    date: string;
+
+    @field({ type: "string" })
+    title: string;
+
+    @field({ type: "string" })
+    imageUrl: string;
+
+    @field({ type: "string"})
+    content: string;
+
+    @field({ type: Documents })
+    messages: Documents<Message>;
+
+    constructor(
+        id: string,
+        date: string,
+        title: string,
+        imageUrl: string,
+        content: string,
+        name?: string,
+    ) {
         super();
-        this.documents = new Documents({
-            id: sha256Sync(new TextEncoder().encode("example-store")),
-        });
+        this.id = id;
+        this.date = date;
+        this.name = name;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.content = content;
+        this.messages =
+            new Documents<Message>({
+                id: sha256Sync(
+                    concat([
+                        new TextEncoder().encode("chat"),
+                        new TextEncoder().encode(this.id)
+                    ])
+                ),
+            });
+
     }
 
-    async open(args?: any): Promise<void> {
-        await this.documents.open({
-            type: SimpleDocument,
+    async open(): Promise<void> {
+        await this.messages.open({
+            type: Message,
+        });
+    }
+}
+
+@variant(0)
+export class IndexableChat {
+
+    @field({ type: "string" })
+    id: string;
+
+    @field({ type: option("string") })
+    name?: string;
+
+    @field({ type: "string" })
+    date: string;
+
+    @field({ type: "string" })
+    imageUrl: string;
+
+    @field({ type: "string" })
+    title: string;
+
+    @field({ type: "string"})
+    message: string;
+
+    @field({ type: "string" })
+    address: string;
+
+    constructor(
+        id: string,
+        date: string,
+        imageUrl: string,
+        title: string,
+        message: string,
+        address: string,
+        name?: string,
+    ) {
+        this.id = id;
+        this.date = date;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.title = title;
+        this.message = message;
+        this.address = address;
+    }
+}
+
+@variant("topic")
+export class Topic extends Program {
+
+    @field({ type: "string" })
+    ticker: string;
+
+    @field({ type: Documents })
+    threads: Documents<Chat, IndexableChat>;
+
+    constructor(
+        ticker: string
+    ) {
+        super();
+        this.ticker = ticker;
+        this.threads =
+            new Documents<Chat, IndexableChat>({
+                id: sha256Sync(
+                    concat([
+                        new TextEncoder().encode("topic"),
+                        new TextEncoder().encode(this.ticker)
+                    ])
+                )
+            });
+    }
+
+    async open(): Promise<void> {
+        await this.threads.open({
+            type: Chat,
+            index: {
+                idProperty: "id",
+                type: IndexableChat,
+                transform: async (thread, _) => {
+                    return new IndexableChat(
+                        thread.id,
+                        thread.date,
+                        thread.imageUrl, 
+                        thread.title, 
+                        thread.content, 
+                        await thread.calculateAddress(),
+                        thread.name
+                    )
+                }
+            },
+            canOpen: (thread) => {
+                return true;
+            }
         });
     }
 }
